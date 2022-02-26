e31e811aba6f6f5c9fad257ef2cdefb3480c3db7
AidenDeVelop <97203807+AidenDeVelop@users.noreply.github.com>
2022-02-26 18:48:44 +0800

    Create Smash Legend.lua

---
+++ Smash Legend.lua
@@ -0,0 +1,34 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Winston Hub | Smash Legends", "Midnight")
+
+-- Create Tab
+local FarmTab = Window:NewTab("Farming")
+local ShopTab = Window:NewTab("Shop")
+
+-- Create Section
+local FarmSection = FarmTab:NewSection("Farming Section")
+local ShopSection = ShopTab:NewSection("Shop Section")
+
+FarmSection:NewToggle("Auto Swing", "ToggleInfo", function(v)
+	_G.autoSwing = v
+	while _G.autoSwing and task.wait() do
+	local Event = game:GetService("ReplicatedStorage").Remotes.HammerSwing
+	Event:FireServer()
+	end
+end)
+
+FarmSection:NewToggle("Auto Sell", "ToggleInfo", function(v)
+	_G.autoSell = v
+	while _G.autoSell and task.wait() do
+	local Event = game:GetService("ReplicatedStorage").Remotes.SellRequest
+	Event:InvokeServer()
+	end
+end)
+
+ShopSection:NewToggle("Auto Buy Hammer", "ToggleInfo", function(v)
+	_G.autoBuy = v
+	while _G.autoBuy and task.wait() do
+	local Event = game:GetService("ReplicatedStorage").Remotes.BuyAllHammers
+	Event:FireServer()
+	end
+end)
